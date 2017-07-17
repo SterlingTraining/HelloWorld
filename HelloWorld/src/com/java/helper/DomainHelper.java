2e0215e48d725934656f0bfb0508068b901b3b64
sterlingtraining2017@gmail.com <sterlingtraining2017@gmail.com>
2017-07-17 18:26:32 -0500

    Added new Helper class with method getDate.

---
+++ HelloWorld/src/com/java/helper/DomainHelper.java
@@ -0,0 +1,10 @@
+package com.java.helper;
+
+public class DomainHelper {
+	
+	public static String getDate(String name) {
+		//SimpleDateFormat sf = new SimpleDateFormat();
+		return name;
+	}
+
+}
--- HelloWorld/src/com/java/service/HelloWorld.java
+++ HelloWorld/src/com/java/service/HelloWorld.java
@@ -1,10 +1,12 @@
 package com.java.service;
 
+import com.java.helper.DomainHelper;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(DomainHelper.getDate("Hello DevOps!!!!"));
 	}
 
 }
